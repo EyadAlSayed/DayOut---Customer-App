7f006b8773f4170d496937799326f98b2b90d325
EYAD <eyadan.900@gmail.com>
2022-04-26 12:03:04 +0300

    building home page

--- app/src/main/java/com/example/dayout/ui/activities/SplashActivity.java
+++ app/src/main/java/com/example/dayout/ui/activities/SplashActivity.java
@@ -3,23 +3,19 @@ package com.example.dayout.ui.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Looper;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.dayout.R;
-
-import java.util.regex.Matcher;
+import com.google.android.material.textfield.TextInputLayout;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class SplashActivity extends AppCompatActivity {
-
 
 
     @BindView(R.id.splash_motto)
@@ -31,6 +27,8 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_activity);
         ButterKnife.bind(this);
+
+
         applyAnimation();
         openMainActivity();
     }
@@ -45,7 +43,7 @@ public class SplashActivity extends AppCompatActivity {
     }
     private void openMainActivity(){
       new Handler(getMainLooper()).postDelayed(() -> {
-          startActivity(new Intent(SplashActivity.this,MainActivity.class));
+          startActivity(new Intent(SplashActivity.this,AuthActivity.class));
           finish();
       },3000);
     }
