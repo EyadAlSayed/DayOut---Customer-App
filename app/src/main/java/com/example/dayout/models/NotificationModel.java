c282fb7ebfad8ceb834c633921a49107d8ee06cc
Caesar <caesarfarah@yahoo.com>
2022-05-22 11:12:05 +0300

    fixes

--- app/src/main/java/com/example/dayout/models/NotificationModel.java
+++ app/src/main/java/com/example/dayout/models/NotificationModel.java
@@ -4,9 +4,4 @@ public class NotificationModel {
 
     public String title;
     public String description;
-
-    public NotificationModel(String title, String description) {
-        this.title = title;
-        this.description = description;
-    }
 }
